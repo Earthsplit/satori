7e5bd383d9ab807268e117e7ec155b0274185195
Earthsplit <codingmonkey148@gmail.com>
2024-04-03 19:55:25 +0200

    fix: change env variables

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@ import { AddCard } from '@/components/AddCard'
 import Cards from '@/components/Cards'
 
 async function getCards() {
-	let url = process.env.URL_DEV
-
-	if (process.env.NODE_ENV == 'production') {
-		url = process.env.URL_PROD
-	}
-
-	const response = await fetch(`${url}/api/cards`, {
+	const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/cards`, {
 		cache: 'no-store',
 	})
 	const data = await response.json()
